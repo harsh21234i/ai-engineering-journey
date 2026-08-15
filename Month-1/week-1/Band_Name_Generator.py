0c5659a1c0ae19b0f3518a308407fda14250a256
Harsh Thakare <harsh@pybex.io>
2026-08-15 18:28:06 +0530

    Band Name Generator

---
+++ Month-1/week-1/Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to my Band Name Generator Project")
+city=input("What's the name of the city you grew up in?\n")
+pet=input("What's the pet name?\n")
+print("So my brand name will be a "+pet+city)
+
