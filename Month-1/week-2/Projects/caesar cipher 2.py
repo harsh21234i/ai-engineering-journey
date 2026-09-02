4e4fa9629a6c55e8ea95d73bc61da9b5bec75546
Harsh Thakare <harshthakare576@gmail.com>
2026-09-02 17:31:21 +0530

    projects

--- Month-1/week-2/Projects/caesar cipher 2.py
+++ Month-1/week-2/Projects/caesar cipher 2.py
@@ -13,10 +13,21 @@ shift = int(input("Type the shift number:\n"))
 
 
 def decrypt(original_text, shift_amount):
-    cipher_text = ""
+    output_text = ""
     for letter in original_text:
         shifted_position = alphabet.index(letter) - shift_amount
         shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the decoded result: {cipher_text}")
-decrypt("harsh", 1)
+        output_text += alphabet[shifted_position]
+    print(f"Here is the decoded result: {output_text}")
+
+
+def caesar_cipher(original_text, shift_amount,encode_or_decode):
+    output_text = ""
+    for letter in original_text:
+        if encode_or_decode == "decode":
+            shift_amount*=1
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode} result: {output_text}")
+caesar_cipher(text,shift,direction)
